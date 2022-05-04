c483cbe7cfb4bb8ed06a620b1ffa69e5819b3c01
박상준 <ray@bagsangjun-ui-MacBookPro.local>
2022-05-04 14:58:55 +0900

    dayjs practice

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import Box from '@mui/material/Box';
+import { styled } from '@mui/system';
+import dayjs from 'dayjs';
 
 interface fakeDBInterface {
+  id: number;
   volume: string;
   upperRatio: string;
   Date: string;
@@ -10,54 +13,73 @@ interface fakeDBInterface {
 
 interface fakeDBInterfaceArray extends Array<fakeDBInterface> {}
 
+let date = dayjs();
+
 const fakeDB: fakeDBInterfaceArray = [
   {
+    id: 1,
     volume: '12.8K',
     upperRatio: '18.87%',
     Date: 'one day ago',
   },
   {
+    id: 2,
     volume: '15.6K',
     upperRatio: '13.33%',
     Date: 'last week',
   },
 
   {
+    id: 3,
     volume: '22.5K',
     upperRatio: '29.31%',
     Date: 'last month',
   },
   {
+    id: 4,
     volume: '79.2K',
     upperRatio: '52.31%',
     Date: 'last week',
   },
   {
+    id: 5,
     volume: '49.2K',
     upperRatio: '12.31%',
     Date: 'two day ago',
   },
   {
+    id: 6,
     volume: '29.2K',
     upperRatio: '33.31%',
     Date: 'two day ago',
   },
   {
+    id: 7,
     volume: '89.2K',
     upperRatio: '42.31%',
     Date: 'three day ago',
   },
 ];
 
+const StyledBox = styled(Box)`
+  box-shadow: 2px 4px 6px gray;
+  transition: 0.3s;
+  &:hover {
+    background-color: rgba(220, 220, 220, 0.5);
+    cursor: pointer;
+    transform: translateY(-3px);
+  }
+`;
+
 function Card(): JSX.Element {
   return (
     <>
       {fakeDB.map((result) => {
         return (
-          <Box
+          <StyledBox
+            key={result.id}
             sx={{
               boxSizing: 'border-box',
-              bgcolor: 'background.paper',
               boxShadow: 3,
               borderRadius: 1,
               p: 3,
@@ -88,7 +110,17 @@ function Card(): JSX.Element {
             <Box sx={{ color: 'text.secondary', display: 'inline', fontSize: 12 }}>
               vs. {result.Date}
             </Box>
-          </Box>
+            <Box
+              sx={{
+                color: 'text.secondary',
+                display: 'block',
+                fontSize: 12,
+                marginTop: 1,
+              }}
+            >
+              {`${date.get('year')}년 ${date.get('month')}월 ${date.get('day')}일`}
+            </Box>
+          </StyledBox>
         );
       })}
     </>
